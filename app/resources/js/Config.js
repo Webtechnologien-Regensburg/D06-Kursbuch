d515ff9567dace8c9e24bae9caed157990b103c1
Alexander Bazo <alexanderbazo@googlemail.com>
2020-07-22 12:23:16 +0200

    Add comments for database module

--- app/resources/js/Config.js
+++ app/resources/js/Config.js
@@ -1,9 +1,9 @@
 const Config = {
-	dbBaseRoute: "http://localhost:8080/db/",
-	courseTemplate: document.querySelector("#class-list-entry-template").innerHTML.trim(),
-	commentTemplate: document.querySelector("#comment-template").innerHTML.trim(),
-	courseList: document.querySelector(".class-list"),
-	courseDetails: document.querySelector(".class-details"),
+    dbBaseRoute: "http://localhost:8080/db/",
+    courseTemplate: document.querySelector("#class-list-entry-template").innerHTML.trim(),
+    commentTemplate: document.querySelector("#comment-template").innerHTML.trim(),
+    courseList: document.querySelector(".class-list"),
+    courseDetails: document.querySelector(".class-details"),
 };
 
 Object.freeze(Config);
